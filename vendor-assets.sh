54d6300eaf0c7679ab2c601d4342b2af2279dc4c
aza <aza@lainventoria.com.ar>
2020-06-17 11:33:11 +0200

    fixing vendor-assets.sh to copy svg

--- vendor-assets.sh
+++ vendor-assets.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ex
 npm install
-cp node_modules/jsoneditor/dist/jsoneditor.{js,css,svg} vendor/assets/jsoneditor/vendored_jsoneditor
+cp node_modules/jsoneditor/dist/jsoneditor*{js,css,svg} vendor/assets/jsoneditor/vendored_jsoneditor
 mkdir -p app/assets/images/trestle/img
 cp -r node_modules/jsoneditor/dist/img app/assets/images/trestle
